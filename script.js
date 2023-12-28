9d7e3bf9a3cfb670b0c9806f66422bc8eacaa3b1
Bibek Modak <90642178+bibek-modak@users.noreply.github.com>
2023-12-29 00:21:26 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
 const inputBox = document.getElementById("input-box");
 const listContainer = document.getElementById("list-container");
+// const cancel = document.getElementById("can");
 
 function addTask(){
     if(inputBox.value ===''){
-        alert("Oops To Do List is empty!!");
+        alert("Enter the reminder first!!");
     }
     else{
         let li=document.createElement("li");
@@ -19,6 +20,7 @@ function addTask(){
     SaveData();
 }
 
+
 listContainer.addEventListener("click",function(e){
     if(e.target.tagName === "LI"){
         e.target.classList.toggle("checked");
@@ -30,6 +32,15 @@ listContainer.addEventListener("click",function(e){
     }
 },false);
 
+
+
+function deleteelem() {
+    if(document.getElementById('list-container').innerHTML == '') window.alert("List is already empty!!");
+ else document.getElementById('list-container').innerHTML = '';
+ SaveData();
+}
+
+
 function SaveData(){
     localStorage.setItem("data", listContainer.innerHTML);
 }
@@ -39,4 +50,3 @@ function showTask(){
 }
 
 showTask();
-reload();
